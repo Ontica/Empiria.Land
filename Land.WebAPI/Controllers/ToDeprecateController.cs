b089fbb40c4e56661593491bc512cc68e0ac70d5
jmcota <jmcota@ontica.org>
2017-03-10 12:17:00 -0500

    Added deprecated web api methods used by Citys (now in microservices)

---
+++ Land.WebAPI/Controllers/ToDeprecateController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Web.Http;
+
+using Empiria.Security;
+using Empiria.WebApi;
+using Empiria.WebApi.Models;
+
+using Empiria.Models.ToDeprecateModels;
+
+namespace Empiria.Land.WebApi {
+
+  /// <summary>Contains almost deprecated services.</summary>
+  public class ToDeprecateController : WebApiController {
+
+    #region Public APIs
+
+    /// <summary>Gets a fomer list of available endpoints for an authenticated user
+    /// in the context of the client application.</summary>
+    /// <returns>A list of former HttpEndpoint objects.</returns>
+    [HttpGet]
+    [Route("v1/system/api-endpoints")]
+    public CollectionModel GetEndpoints() {
+      try {
+        var endpoints = HttpEndpoint.GetList();
+        return new CollectionModel(base.Request, endpoints);
+      } catch (Exception e) {
+        throw base.CreateHttpException(e);
+      }
+    }
+
+    [HttpPost, AllowAnonymous]
+    [Route("v1/security/login")]
+    public SingleObjectModel Login(LoginModel login)
+    {
+      try
+      {
+        base.RequireHeader("User-Agent");
+        base.RequireBody(login);
+
+        EmpiriaPrincipal principal = this.GetPrincipal(login);
+
+        return new SingleObjectModel(base.Request, LoginModel.ToOAuth(principal),
+                                     "Empiria.Security.OAuthObject");
+      }
+      catch (Exception e)
+      {
+        throw base.CreateHttpException(e);
+      }
+    }
+
+    #endregion Public APIs
+
+
+    #region Private methods
+
+    private EmpiriaPrincipal GetPrincipal(LoginModel login) {
+      login.AssertValid();
+
+      EmpiriaPrincipal principal = AuthenticationHttpModule.Authenticate(login.api_key,
+                                                                         login.user_name,
+                                                                         login.password);
+      Assertion.AssertObject(principal, "principal");
+
+      return principal;
+    }
+
+    #endregion Private methods
+
+  }  // class ToDeprecateController
+
+}  // namespace Empiria.Land.WebApi
---
+++ Land.WebAPI/Models/LoginModel.cs
@@ -0,0 +1,65 @@
+﻿/* Empiria Extensions Framework ******************************************************************************
+*                                                                                                            *
+*  Solution  : Empiria Extensions Framework                     System   : Empiria Microservices             *
+*  Namespace : Empiria.Microservices                            Assembly : Empiria.Microservices.dll         *
+*  Type      : LoginModel                                       Pattern  : Information Holder                *
+*  Version   : 1.0                                              License  : Please read license.txt file      *
+*                                                                                                            *
+*  Summary   : Holds information to log in a user into the web api system.                                   *
+*                                                                                                            *
+********************************** Copyright(c) 2016-2017. La Vía Óntica SC, Ontica LLC and contributors.  **/
+using Empiria.Security;
+
+namespace Empiria.Models.ToDeprecateModels {
+
+  /// <summary>Holds information to log in a user into the web api system.
+  /// This model was moved to microservices, so it must be deprecated from here.</summary>
+  public class LoginModel {
+
+    #region Properties
+
+    public string api_key {
+      get;
+      set;
+    }
+
+    public string user_name {
+      get;
+      set;
+    }
+
+    public string password {
+      get;
+      set;
+    }
+
+    #endregion Properties
+
+    #region Methods
+
+    public void AssertValid() {
+      Assertion.AssertObject(api_key, "api_key");
+      Assertion.AssertObject(user_name, "user_name");
+      Assertion.AssertObject(password, "password");
+    }
+
+    static public object ToOAuth(EmpiriaPrincipal principal) {
+
+      return new {
+        access_token = principal.Session.Token,
+        token_type = principal.Session.TokenType,
+        expires_in = principal.Session.ExpiresIn,
+        refresh_token = principal.Session.RefreshToken,
+        user = new {
+          uid = principal.Identity.User.Id,
+          username = principal.Identity.User.UserName,
+          email = principal.Identity.User.EMail
+        }
+      };
+    }
+
+    #endregion Methods
+
+  }  // class LoginModel
+
+} // namespace Empiria.Microservices
