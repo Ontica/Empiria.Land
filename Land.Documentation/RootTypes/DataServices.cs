de1cc55811018a47f051d2ff9b7ae5af0d2e7e96
jmcota <jmcota@ontica.org>
2019-04-08 23:48:28 -0400

    Add DocumentUploader service with its related domain types

--- Land.Documentation/RootTypes/DataServices.cs
+++ Land.Documentation/RootTypes/DataServices.cs
@@ -1,26 +1,28 @@
 ﻿/* Empiria Land **********************************************************************************************
 *                                                                                                            *
-*  Solution  : Empiria Land                                   System   : Land Registration System            *
-*  Namespace : Empiria.Land.Documentation                     Assembly : Empiria.Land.Documentation          *
-*  Type      : DataServices                                   Pattern  : Data Services Static Class          *
-*  Version   : 3.0                                            License  : Please read license.txt file        *
+*  Module   : Land Documentation                           Component : Document Management                   *
+*  Assembly : Empiria.Land.Documentation.dll               Pattern   : Data Services                         *
+*  Type     : DataServices                                 License   : Please read LICENSE.txt file          *
 *                                                                                                            *
-*  Summary   : Database read and write services specific for the Land Documentation component.               *
+*  Summary  : Database read and write services specific for the Land Documentation component.                *
 *                                                                                                            *
-********************************* Copyright (c) 2009-2017. La Vía Óntica SC, Ontica LLC and contributors.  **/
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
 using System;
 
 using Empiria.Data;
 using Empiria.Land.Registration;
+using Empiria.Land.Registration.Transactions;
 
 namespace Empiria.Land.Documentation {
 
   /// <summary>Database read and write services specific for the Land Documentation component.</summary>
   static internal class DataServices {
 
-    #region Public methods
+    #region Read methods
 
-    static internal bool DocumentWasDigitalized(RecordingDocument document, DocumentImageType imageType) {
+
+    static internal bool DocumentWasDigitalized(RecordingDocument document,
+                                                DocumentImageType imageType) {
       string sql = "SELECT * FROM LRSImagingItems " +
                    "WHERE DocumentId = {0} AND ImageType = '{1}'";
 
@@ -30,12 +32,30 @@ namespace Empiria.Land.Documentation {
     }
 
 
+    static internal FixedList<TransactionDocument> GetTransactionDocuments(LRSTransaction transaction) {
+      string sql = $"SELECT * FROM LRSImagingItems " +
+                   $"WHERE TransactionId = {transaction.Id} " +
+                   $"AND ImagingItemTypeId = 2004 AND ImagingItemStatus <> 'X'";
+
+      var op = DataOperation.Parse(sql);
+
+      return DataReader.GetFixedList<TransactionDocument>(op);
+    }
+
+
+    #endregion Read methods
+
+    #region Write methods
+
+
     static internal void WriteImagingItem(DocumentImageSet o) {
-      var operation = DataOperation.Parse("writeLRSImagingItem", o.Id, o.GetEmpiriaType().Id, o.Document.Id,
-                                          RecordingBook.Empty.Id, (char) o.DocumentImageType,
-                                          o.BaseFolder.Id, o.ItemPath, o.ImagingItemExtData.ToString(),
-                                          o.FilesCount, o.Integrity.GetUpdatedHashCode());
-      DataWriter.Execute(operation);
+      var op = DataOperation.Parse("writeLRSImagingItem", o.Id, o.GetEmpiriaType().Id,
+                                   o.Document.GetTransaction().Id, o.Document.Id,
+                                   RecordingBook.Empty.Id, (char) o.DocumentImageType,
+                                   o.BaseFolder.Id, o.ItemPath, o.ImagingItemExtData.ToString(),
+                                   o.FilesCount, o.DigitalizedBy.Id, o.DigitalizationDate,
+                                   (char) o.Status, o.Integrity.GetUpdatedHashCode());
+      DataWriter.Execute(op);
     }
 
 
@@ -56,6 +76,17 @@ namespace Empiria.Land.Documentation {
                                    DateTime.Now, message, o.Document.Id, -1,
                                    o.SourceFile.DirectoryName, 'E', exception.ToString());
 
+      DataWriter.Execute(op);
+    }
+
+
+    static internal void WriteTransactionDocument(TransactionDocument o) {
+      var op = DataOperation.Parse("writeLRSImagingItem", o.Id, o.GetEmpiriaType().Id,
+                                   o.Transaction.Id, o.Transaction.Document.Id,
+                                   RecordingBook.Empty.Id, (char) o.DocumentType,
+                                   o.BaseFolder.Id, o.ItemPath, o.ImagingItemExtData.ToString(),
+                                   o.FilesCount, o.DigitalizedBy.Id, o.DigitalizationDate,
+                                   (char) o.Status, o.Integrity.GetUpdatedHashCode());
       DataWriter.Execute(op);
     }
 
---
+++ Land.Documentation/RootTypes/DocumentUploader.cs
@@ -0,0 +1,72 @@
+﻿/* Empiria Land **********************************************************************************************
+*                                                                                                            *
+*  Module   : Land Documentation                         Component : Document Uploader                       *
+*  Assembly : Empiria.Land.Registration.dll              Pattern   : Service provider                        *
+*  Type     : DocumentUploader                           License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Attaches documents to land transactions or recording documents.                                *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Web;
+
+using Empiria.Documents;
+
+using Empiria.Land.Registration.Transactions;
+
+namespace Empiria.Land.Documentation {
+
+  /// <summary>Attaches documents to land transactions or recording documents.</summary>
+  static public class DocumentUploader {
+
+    #region Public methods
+
+
+    static public TransactionDocument SetMainDocument(LRSTransaction transaction, HttpPostedFile uploadedFile) {
+      Assertion.AssertObject(transaction, "transaction");
+      Assertion.AssertObject(uploadedFile, "uploadedFile");
+      Assertion.Assert(uploadedFile.ContentLength > 0, "uploadedFile is an empty file.");
+
+      var documentSet = TransactionDocumentSet.ParseFor(transaction);
+
+      documentSet.SetMainDocument(uploadedFile.InputStream, GetFileType(uploadedFile), uploadedFile.FileName);
+      return documentSet.MainDocument;
+    }
+
+
+    static public TransactionDocument SetAuxiliaryDocument(LRSTransaction transaction, HttpPostedFile uploadedFile) {
+      Assertion.AssertObject(transaction, "transaction");
+      Assertion.AssertObject(uploadedFile, "uploadedFile");
+      Assertion.Assert(uploadedFile.ContentLength > 0, "uploadedFile is an empty file.");
+
+      var documentSet = TransactionDocumentSet.ParseFor(transaction);
+
+      documentSet.SetAuxiliaryDocument(uploadedFile.InputStream, GetFileType(uploadedFile), uploadedFile.FileName);
+
+      return documentSet.AuxiliaryDocument;
+    }
+
+
+    #endregion Public methods
+
+
+    #region Private methods
+
+
+    static private FileContentType GetFileType(HttpPostedFile uploadedFile) {
+      switch (uploadedFile.ContentType) {
+        case "application/pdf":
+          return FileContentType.PDF;
+
+        default:
+          throw Assertion.AssertNoReachThisCode($"The system can't handle uploaded files " +
+                                                $"with content type {uploadedFile.ContentType}.");
+      }
+    }
+
+    #endregion Private methods
+
+
+  }  // class DocumentUploader
+
+}  // namespace Empiria.Land.Documentation
