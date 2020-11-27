a8f044eeeafdd6526567884f61c617fe8debd22d
José Manuel Cota <jmcota@ontica.org>
2020-11-27 11:28:38 -0500

    Empiria Land Use Cases Layer Module added

---
+++ Land.UseCases/Empiria.Land.UseCases.AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿/* Empiria Land **********************************************************************************************
+*                                                                                                            *
+*  Module   : Empiria Land Use Cases                     Component : Use Cases Layer                         *
+*  Assembly : Empiria.Land.UseCases.dll                  Pattern   : Assembly Attributes File                *
+*  Type     : None                                       License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Empiria Land Use Cases Layer Module.                                                           *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Reflection;
+
+/*************************************************************************************************************
+* Assembly configuration attributes.                                                                         *
+*************************************************************************************************************/
+[assembly: AssemblyTrademark("Empiria and Ontica are either trademarks of La Vía Óntica SC or Ontica LLC.")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
