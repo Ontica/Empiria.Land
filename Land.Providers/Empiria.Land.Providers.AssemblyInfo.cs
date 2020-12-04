dd2f4f18d9cf6a0027a7455aa43145026a5654d1
José Manuel Cota <jmcota@ontica.org>
2020-12-04 12:38:17 -0500

    Empiria Land Providers module added

---
+++ Land.Providers/Empiria.Land.Providers.AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿/* Empiria Land **********************************************************************************************
+*                                                                                                            *
+*  Module   : Empiria Land Providers                     Component : Integration Layer                       *
+*  Assembly : Empiria.Land.Providers.dll                 Pattern   : Assembly Attributes File                *
+*  Type     : None                                       License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Provides pluggable external services for Empiria Land.                                         *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Reflection;
+
+/*************************************************************************************************************
+* Assembly configuration attributes.                                                                         *
+*************************************************************************************************************/
+[assembly: AssemblyTrademark("Empiria and Ontica are either trademarks of La Vía Óntica SC or Ontica LLC.")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
---
+++ Land.Providers/README.md
@@ -0,0 +1,11 @@
+﻿# Empiria Land Providers
+
+Provides pluggable external services for Empiria Land.
+
+## License
+
+This system is distributed by the GNU AFFERO GENERAL PUBLIC LICENSE.
+
+## Copyright
+
+Copyright © 2009-2021. La Vía Óntica SC, Ontica LLC and colaborators.
