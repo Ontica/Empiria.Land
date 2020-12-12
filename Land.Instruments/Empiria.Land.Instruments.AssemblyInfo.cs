99f422ac04d522238700107b8f6b700bd91ccc87
José Manuel Cota <jmcota@ontica.org>
2020-12-12 10:20:41 -0500

    Empiria Land Legal Instruments Module added

---
+++ Land.Instruments/Empiria.Land.Instruments.AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿/* Empiria Land **********************************************************************************************
+*                                                                                                            *
+*  Module   : Empiria Land Legal Instruments             Component : Domain Layer                            *
+*  Assembly : Empiria.Land.Instruments.dll               Pattern   : Assembly Attributes File                *
+*  Type     : None                                       License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Empiria Land Legal Instruments Module.                                                         *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Reflection;
+
+/*************************************************************************************************************
+* Assembly configuration attributes.                                                                         *
+*************************************************************************************************************/
+[assembly: AssemblyTrademark("Empiria and Ontica are either trademarks of La Vía Óntica SC or Ontica LLC.")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
---
+++ Land.Instruments/README.md
@@ -0,0 +1,11 @@
+﻿# Empiria Land Legal Instruments
+
+Empiria Land Legal Instruments Module.
+
+## License
+
+This system is distributed by the GNU AFFERO GENERAL PUBLIC LICENSE.
+
+## Copyright
+
+Copyright © 2009-2021. La Vía Óntica SC, Ontica LLC and colaborators.
