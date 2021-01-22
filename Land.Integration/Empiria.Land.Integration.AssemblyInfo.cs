e00ff3ac82bc2e22a351d6c17e09cd173e28d5a2
José Manuel Cota <jmcota@ontica.org>
2021-01-21 22:39:14 -0500

    Land.Integration module created to integrate with external services

---
+++ Land.Integration/Empiria.Land.Integration.AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿/* Empiria Land **********************************************************************************************
+*                                                                                                            *
+*  Module   : Empiria Land Integration Interfaces        Component : Integration Layer                       *
+*  Assembly : Empiria.Land.Integration.dll               Pattern   : Assembly Attributes File                *
+*  Type     : None                                       License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Defines interfaces and adapters used to integrate Empiria Land with external services.         *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Reflection;
+
+/*************************************************************************************************************
+* Assembly configuration attributes.                                                                         *
+*************************************************************************************************************/
+[assembly: AssemblyTrademark("Empiria and Ontica are either trademarks of La Vía Óntica SC or Ontica LLC.")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
