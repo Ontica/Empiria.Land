c5dab88eb574c8539e15f09936a334217eece574
José Manuel Cota <jmcota@ontica.org>
2021-02-18 22:16:50 -0500

    Instruments control data flags structure added

---
+++ Land.Core/Instruments/Adapters/InstrumentControlDataDto.cs
@@ -0,0 +1,98 @@
+﻿/* Empiria Land **********************************************************************************************
+*                                                                                                            *
+*  Module   : Legal Instruments                          Component : Interface adapters                      *
+*  Assembly : Empiria.Land.Core.dll                      Pattern   : Data Transfer Object                    *
+*  Type     : InstrumentControlDataDto                   License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Output DTO that holds legal instrument control data flags.                                     *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+namespace Empiria.Land.Instruments.Adapters {
+
+  /// <summary>Output DTO that holds legal instrument control data flags.</summary>
+  public class InstrumentControlDataDto {
+
+    public InstrumentCanControlData Can {
+      get; private set;
+    } = new InstrumentCanControlData();
+
+
+    public InstrumentShowControlData Show {
+      get; private set;
+    } = new InstrumentShowControlData();
+
+  }  // class InstrumentControlDataDto
+
+
+  /// <summary>Output DTO that holds the 'Can' part of control data flags.</summary>
+  public class InstrumentCanControlData {
+
+    public bool Edit {
+      get; internal set;
+    }
+
+    public bool Open {
+      get; internal set;
+    }
+
+    public bool Close {
+      get; internal set;
+    }
+
+    public bool Delete {
+      get; internal set;
+    }
+
+    public bool UploadFiles {
+      get; internal set;
+    }
+
+    public bool EditRecordingActs {
+      get; internal set;
+    }
+
+    public bool CreatePhysicalRecordings {
+      get; internal set;
+    }
+
+    public bool DeletePhysicalRecordings {
+      get; internal set;
+    }
+
+    public bool LinkPhysicalRecordings {
+      get; internal set;
+    }
+
+    public bool EditPhysicalRecordingActs {
+      get; internal set;
+    }
+
+  }  // class InstrumentCanControlData
+
+
+
+  /// <summary>Output DTO that holds the 'Show' part of control data flags.</summary>
+  public class InstrumentShowControlData {
+
+    public bool Files {
+      get; internal set;
+    }
+
+    public bool RecordingActs {
+      get; internal set;
+    }
+
+    public bool PhysicalRecordings {
+      get; internal set;
+    }
+
+    public bool RegistrationStamps {
+      get;
+      internal set;
+    }
+
+  }  // class InstrumentShowControlData
+
+}  // namespace Empiria.Land.Transactions.Adapters
---
+++ Land.Core/Instruments/Entities/InstrumentControlData.cs
@@ -0,0 +1,133 @@
+﻿/* Empiria Land **********************************************************************************************
+*                                                                                                            *
+*  Module   : Legal Instruments                          Component : Domain Layer                            *
+*  Assembly : Empiria.Land.Core.dll                      Pattern   : Control data class                      *
+*  Type     : InstrumentControlData                      License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Provides edition and other control data for a legal instrument for the current user.           *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+namespace Empiria.Land.Instruments {
+
+  internal class InstrumentControlData {
+
+    private readonly Instrument _instrument;
+
+    internal InstrumentControlData(Instrument instrument) {
+      _instrument = instrument;
+    }
+
+    public bool CanDelete {
+      get {
+        return false;
+      }
+    }
+
+
+    public bool CanEdit {
+      get {
+        return true;
+      }
+    }
+
+
+    public bool CanOpen {
+      get {
+        return false;
+      }
+    }
+
+
+    public bool CanClose {
+      get {
+        return false;
+      }
+    }
+
+
+    public bool CanMarkAsNoRecordable {
+      get {
+        return false;
+      }
+    }
+
+
+    public bool CanProceedWithRecording {
+      get {
+        return false;
+      }
+    }
+
+
+    public bool CanEditRecordingActs {
+      get {
+        return false;
+      }
+    }
+
+
+    public bool CanCreatePhysicalRecordings {
+      get {
+        return (_instrument.TryGetRecordingDocument() != null);
+      }
+    }
+
+
+    public bool CanDeletePhysicalRecordings {
+      get {
+        return (_instrument.HasPhysicalRecordings);
+      }
+    }
+
+
+    public bool CanLinkPhysicalRecordings {
+      get {
+        return false;
+      }
+    }
+
+
+    public bool CanEditPhysicalRecordingActs {
+      get {
+        return false;
+      }
+    }
+
+
+    public bool CanUploadFiles {
+      get {
+        return false;
+      }
+    }
+
+
+    public bool ShowFiles {
+      get {
+        return false;
+      }
+    }
+
+
+    public bool ShowPhysicalRecordings {
+      get {
+        return (_instrument.TryGetRecordingDocument() != null);
+      }
+    }
+
+
+    public bool ShowRecordingActs {
+      get {
+        return false;
+      }
+    }
+
+    public bool ShowRegistrationStamps {
+      get {
+        return _instrument.HasPhysicalRecordings;
+      }
+    }
+
+  }  // class InstrumentControlData
+
+}  // namespace Empiria.Land.Instruments
