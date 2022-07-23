cab08ca7848fe18fcee6ae88e448a727945ba5dc
José Manuel Cota <jmcota@ontica.org>
2022-07-22 23:27:46 -0400

    Rfx Rename WorkflowPayload to WorkflowCommandPayload

--- Land.Core/Workflow/Adapters/WorkflowCommand.cs
+++ Land.Core/Workflow/Adapters/WorkflowCommand.cs
@@ -9,6 +9,8 @@
 ************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
 using System;
 
+using Empiria.Contacts;
+
 using Empiria.Land.Transactions.Adapters;
 
 namespace Empiria.Land.Workflow.Adapters {
@@ -16,46 +18,62 @@ namespace Empiria.Land.Workflow.Adapters {
   /// <summary>Contains information about a transaction workflow command than can be executed.</summary>
   public class WorkflowCommand {
 
+    #region Fields
+
     public WorkflowCommandType Type {
       get; set;
     } = WorkflowCommandType.Undefined;
 
 
-    public WorkflowPayload Payload {
+    public WorkflowCommandPayload Payload {
       get; set;
-    } = new WorkflowPayload();
+    } = new WorkflowCommandPayload();
+
+    #endregion Fields
+
+
+    /// <summary>Holds data needed to execute a given workflow command.</summary>
+    public class WorkflowCommandPayload {
+
+      internal WorkflowCommandPayload() {
+        // no-op
+      }
+
+      public string[] TransactionUID {
+        get; set;
+      } = new string[0];
+
+
+      public string AssignToUID {
+        get; set;
+      } = string.Empty;
+
+
+      public TransactionStatus NextStatus {
+        get; set;
+      } = TransactionStatus.Undefined;
+
+
+      public string SearchUID {
+        get; set;
+      } = string.Empty;
+
+
+      public string Note {
+        get; set;
+      } = string.Empty;
+
+
+      internal Contact AssignTo() {
+        if (!string.IsNullOrWhiteSpace(this.AssignToUID)) {
+          return Contact.Parse(this.AssignToUID);
+        }
+        return Contact.Empty;
+      }
+
+    }  // inner class WorkflowCommandPayload
+
 
   }  // class WorkflowCommand
-
-
-  /// <summary>Holds data needed to execute a given workflow command.</summary>
-  public class WorkflowPayload {
-
-    public string[] TransactionUID {
-      get; set;
-    } = new string[0];
-
-
-    public string AssignToUID {
-      get; set;
-    } = string.Empty;
-
-
-    public TransactionStatus NextStatus {
-      get; set;
-    } = TransactionStatus.Undefined;
-
-
-    public string SearchUID {
-      get; set;
-    } = string.Empty;
-
-
-    public string Note {
-      get; set;
-    } = string.Empty;
-
-
-  }  // class WorkflowPayload
 
 }  // namespace Empiria.Land.Workflow.Adapters
