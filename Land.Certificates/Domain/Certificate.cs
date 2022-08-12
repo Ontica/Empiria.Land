9e456ea6760e20b479a229fb960359a50fb4492d
José Manuel Cota <jmcota@ontica.org>
2022-08-11 22:20:30 -0400

    CertificateType and Certificate types were added

---
+++ Land.Certificates/Domain/Certificate.cs
@@ -0,0 +1,87 @@
+﻿/* Empiria Land **********************************************************************************************
+*                                                                                                            *
+*  Module   : Certificates Issuing                       Component : Domain Layer                            *
+*  Assembly : Empiria.Land.Certificates.dll              Pattern   : Partitioned type                        *
+*  Type     : Certificate                                License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Partitioned type that represents a Land certificate.                                           *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+using Empiria.Ontology;
+using Empiria.Security;
+
+namespace Empiria.Land.Certificates {
+
+  /// <summary>Partitioned type that represents a Land certificate.</summary>
+  [PartitionedType(typeof(CertificateType))]
+  internal partial class Certificate : BaseObject, IProtected {
+
+    #region Constructors and parsers
+
+    private Certificate(CertificateType powerType) : base(powerType) {
+      // Required by Empiria Framework for all partitioned types.
+    }
+
+    static public Certificate Parse(int id) {
+      return BaseObject.ParseId<Certificate>(id);
+    }
+
+    #endregion Constructors and parsers
+
+    #region Properties
+
+    public CertificateType CertificateType {
+      get {
+        return (CertificateType) base.GetEmpiriaType();
+      }
+    }
+
+   #endregion Properties
+
+    #region IProtected implementation
+
+    int IProtected.CurrentDataIntegrityVersion {
+      get {
+        return 1;
+      }
+    }
+
+
+    private IntegrityValidator _validator = null;
+    IntegrityValidator IProtected.Integrity {
+      get {
+        if (_validator == null) {
+          _validator = new IntegrityValidator(this);
+        }
+        return _validator;
+      }
+    }
+
+
+    object[] IProtected.GetDataIntegrityFieldValues(int version) {
+      if (version == 1) {
+        return new object[] {
+          version, "Id", this.Id, "CertificateTypeId", this.CertificateType.Id,
+          "UID", this.UID
+        };
+      }
+      throw new SecurityException(SecurityException.Msg.WrongDIFVersionRequested, version);
+    }
+
+
+    public IntegrityValidator Integrity {
+      get {
+        if (_validator == null) {
+          _validator = new IntegrityValidator(this);
+        }
+        return _validator;
+      }
+    }
+
+    #endregion IProtected implementation
+
+  } // class Certificate
+
+} // namespace Empiria.Land.Certificates
---
+++ Land.Certificates/Domain/CertificateType.cs
@@ -0,0 +1,61 @@
+﻿/* Empiria Land **********************************************************************************************
+*                                                                                                            *
+*  Module   : Certificates Issuing                       Component : Domain Layer                            *
+*  Assembly : Empiria.Land.Certificates.dll              Pattern   : Powertype                               *
+*  Type     : CertificateType                            License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Powertype that defines a Land certificate type.                                                *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+using Empiria.Ontology;
+
+namespace Empiria.Land.Certificates {
+
+  ///<summary>Power type that describes certificates types.</summary>
+  [Powertype(typeof(Certificate))]
+  internal sealed class CertificateType : Powertype {
+
+    #region Constructors and parsers
+
+    private CertificateType() {
+      // Empiria powertype types always have this constructor.
+    }
+
+
+    static internal new CertificateType Parse(int typeId) {
+      return ObjectTypeInfo.Parse<CertificateType>(typeId);
+    }
+
+
+    static internal new CertificateType Parse(string typeName) {
+      return ObjectTypeInfo.Parse<CertificateType>(typeName);
+    }
+
+
+    static internal FixedList<CertificateType> GetList() {
+      GeneralList list = GeneralList.Parse("CertificateTypes.Default.List");
+
+      return list.GetItems<CertificateType>();
+    }
+
+
+    static internal CertificateType Empty => CertificateType.Parse("ObjectType.LandCertificate");
+
+
+    #endregion Constructors and parsers
+
+    #region Properties
+
+    internal string HtmlTemplateFileName {
+      get {
+       return base.ExtensionData.Get<String>("HtmlTemplateFileName");
+      }
+    }
+
+    #endregion Properties
+
+  } // class CertificateType
+
+} // namespace Empiria.Land.Certificates
