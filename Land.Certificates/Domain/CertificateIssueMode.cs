764f723ed98ceef4fed9caf82cfd547a0d27c4e5
José Manuel Cota <jmcota@ontica.org>
2022-08-13 12:12:04 -0400

    Certificates related enumerations added

---
+++ Land.Certificates/Domain/CertificateIssueMode.cs
@@ -0,0 +1,23 @@
+﻿/* Empiria Land **********************************************************************************************
+*                                                                                                            *
+*  Module   : Certificates Issuing                       Component : Domain Layer                            *
+*  Assembly : Empiria.Land.Certificates.dll              Pattern   : Enumeration                             *
+*  Type     : CertificateIssueMode                       License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Enumerates the issuing mode of a certificate.                                                  *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+namespace Empiria.Land.Certificates {
+
+  /// <summary>Enumerates the issuing mode of a certificate.</summary>
+  public enum CertificateIssueMode {
+
+    Automatic = 'A',
+
+    Manual = 'M',
+
+  }  // CertificateIssueMode
+
+} // namespace Empiria.Land.Certification
---
+++ Land.Certificates/Domain/CertificateStatus.cs
@@ -0,0 +1,54 @@
+﻿/* Empiria Land **********************************************************************************************
+*                                                                                                            *
+*  Module   : Certificates Issuing                       Component : Domain Layer                            *
+*  Assembly : Empiria.Land.Certificates.dll              Pattern   : Enumeration                             *
+*  Type     : CertificateStatus                          License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Describes the status of a land certificate.                                                    *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+namespace Empiria.Land.Certificates {
+
+  /// <summary>Describes the status of a land certificate.</summary>
+  public enum CertificateStatus {
+
+    Pending = 'P',
+
+    Closed = 'C',
+
+    Deleted = 'X',
+
+    Canceled = 'L',
+
+  }  // enum CertificateStatus
+
+
+
+  /// <summary>Extension methods for CertificateStatus enumeration.</summary>
+  static public class CertificateStatusExtensions {
+
+    static public string Name(this CertificateStatus certificateStatus) {
+      switch (certificateStatus) {
+
+        case CertificateStatus.Canceled:
+          return "Cancelado";
+
+        case CertificateStatus.Closed:
+          return "Cerrado";
+
+        case CertificateStatus.Deleted:
+          return "Eliminado";
+
+        case CertificateStatus.Pending:
+          return "En elaboración";
+
+        default:
+          throw Assertion.EnsureNoReachThisCode($"Unrecognized certificate status {certificateStatus}.");
+      }
+    }
+
+  }  // class CertificateStatusExtensions
+
+} // namespace Empiria.Land.Certification
