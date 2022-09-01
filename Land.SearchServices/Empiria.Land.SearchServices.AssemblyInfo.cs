29c9994eaf05942647c137a12ace560f7a9280ec
José Manuel Cota <jmcota@ontica.org>
2022-09-01 14:38:47 -0400

    Land records search services module added

---
+++ Land.SearchServices/Empiria.Land.SearchServices.AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿/* Empiria Land **********************************************************************************************
+*                                                                                                            *
+*  Module   : Search services                             Component : Domain Layer                           *
+*  Assembly : Empiria.Land.SearchServices.dll             Pattern   : Assembly Attributes File               *
+*  Type     : None                                        License   : Please read LICENSE.txt file           *
+*                                                                                                            *
+*  Summary  : Provides search services on Land records for internal and external uses.                       *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Reflection;
+
+/*************************************************************************************************************
+* Assembly configuration attributes.                                                                         *
+*************************************************************************************************************/
+[assembly: AssemblyTrademark("Empiria and Ontica are either trademarks of La Vía Óntica SC or Ontica LLC.")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
