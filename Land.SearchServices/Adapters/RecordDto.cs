31d9752476315f931adcfcc133aa3438c0d96ab3
José Manuel Cota <jmcota@ontica.org>
2022-09-01 22:17:46 -0400

    RecordDto and RecordMapper added

---
+++ Land.SearchServices/Adapters/RecordDto.cs
@@ -0,0 +1,68 @@
+﻿/* Empiria Land **********************************************************************************************
+*                                                                                                            *
+*  Module   : Search services                            Component : Interface adapters                      *
+*  Assembly : Empiria.Land.SearchServices.dll            Pattern   : Data Transfer Object                    *
+*  Type     : RecordDto                                  License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Describes a land system electronic record.                                                     *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+namespace Empiria.Land.SearchServices {
+
+  /// <summary>Describes a land system electronic record.</summary>
+  public class RecordDto {
+
+    public string UID {
+      get; internal set;
+    }
+
+
+    public string ElectronicID {
+      get; internal set;
+    }
+
+
+    public string RecorderOffice {
+      get; internal set;
+    }
+
+
+    public string InstrumentType {
+      get; internal set;
+    }
+
+
+    public DateTime PresentationTime {
+      get; internal set;
+    }
+
+
+    public DateTime RecordingTime {
+      get; internal set;
+    }
+
+
+    public string RecordedBy {
+      get; internal set;
+    }
+
+
+    public string SignedBy {
+      get; internal set;
+    }
+
+
+    public string BookEntry {
+      get; internal set;
+    }
+
+
+    public string TransactionID {
+      get; internal set;
+    }
+
+  }  // class RecordDto
+
+}  // namespace Empiria.Land.SearchServices
---
+++ Land.SearchServices/Adapters/RecordMapper.cs
@@ -0,0 +1,38 @@
+﻿/* Empiria Land **********************************************************************************************
+*                                                                                                            *
+*  Module   : Search services                            Component : Interface adapters                      *
+*  Assembly : Empiria.Land.SearchServices.dll            Pattern   : Mapper class                            *
+*  Type     : RecordMapper                               License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Methods used to map land electronic records.                                                   *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+using Empiria.Land.Registration;
+
+namespace Empiria.Land.SearchServices {
+
+  /// <summary>Methods used to map land electronic records.</summary>
+  static internal class RecordMapper {
+
+    static internal RecordDto Map(RecordingDocument document) {
+      PhysicalRecording bookEntry = document.TryGetHistoricRecording();
+
+      return new RecordDto {
+        UID = document.GUID,
+        ElectronicID = document.UID,
+        RecorderOffice = document.RecorderOffice.Alias,
+        InstrumentType = document.DocumentType.DisplayName,
+        PresentationTime = document.PresentationTime,
+        RecordingTime = document.AuthorizationTime,
+        RecordedBy = document.PostedBy.Alias,
+        SignedBy = document.AuthorizedBy.Alias,
+        BookEntry = bookEntry != null ? bookEntry.AsText : String.Empty,
+        TransactionID = document.TransactionID,
+      };
+    }
+
+  }  // class RecordMapper
+
+}  // namespace Empiria.Land.SearchServices
