6549fc319cdf97d7b24244c475914cd6033f6959
José Manuel Cota <jmcota@ontica.org>
2022-09-17 19:35:10 -0500

    RecordingDataDto type with its mapper class was added

---
+++ Land.Core/Registration/Adapters/RecordingDataDto.cs
@@ -0,0 +1,120 @@
+﻿/* Empiria Land **********************************************************************************************
+*                                                                                                            *
+*  Module   : Recordable Subjects                        Component : Interface adapters                      *
+*  Assembly : Empiria.Land.Core.dll                      Pattern   : Data Transfer Object                    *
+*  Type     : RecordingDataDto                           License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Output DTO with recording data for a real property and other kinds of recordable subjects.     *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+using Newtonsoft.Json;
+
+using Empiria.DataTypes;
+
+namespace Empiria.Land.Registration.Adapters {
+
+  /// <summary>Output DTO with recording data for a real property and other
+  /// kinds of recordable subjects.</summary>
+  public class RecordingDataDto {
+
+    internal RecordingDataDto() {
+      // no-op
+    }
+
+    public string UID {
+      get; internal set;
+    }
+
+
+    public string Type {
+      get; internal set;
+    }
+
+
+    public string RecordingID {
+      get; internal set;
+    }
+
+
+    public string Description {
+      get; internal set;
+    }
+
+
+    public NamedEntityDto Office {
+      get; internal set;
+    }
+
+
+    [JsonProperty(PropertyName = "BookEntry",
+                  NullValueHandling = NullValueHandling.Include)]
+    public VolumeEntryDataDto VolumeEntryData {
+      get; internal set;
+    }
+
+
+    public DateTime RecordingTime {
+      get; internal set;
+    }
+
+
+    public string RecordedBy {
+      get; internal set;
+    }
+
+
+    public string AuthorizedBy {
+      get; internal set;
+    }
+
+
+    public DateTime PresentationTime {
+      get; internal set;
+    }
+
+
+    public string TransactionUID {
+      get; internal set;
+    }
+
+
+    public string Status {
+      get; internal set;
+    }
+
+
+    public MediaData Media {
+      get; internal set;
+    }
+
+  }  // class RecordingDataDto
+
+
+
+  /// <summary>Contains data that describes a physical volume entry.</summary>
+  public class VolumeEntryDataDto {
+
+    internal VolumeEntryDataDto() {
+      // no-op
+    }
+
+    public string UID {
+      get; internal set;
+    }
+
+
+    public string RecordingBookUID {
+      get; internal set;
+    }
+
+
+    public string InstrumentRecordingUID {
+      get; internal set;
+    }
+
+  }  // class VolumeEntryDataDto
+
+
+}  // namespace Empiria.Land.Registration.Adapters
---
+++ Land.Core/Registration/Adapters/RecordingDataMapper.cs
@@ -0,0 +1,60 @@
+﻿/* Empiria Land **********************************************************************************************
+*                                                                                                            *
+*  Module   : Registration Services                      Component : Interface adapters                      *
+*  Assembly : Empiria.Land.Core.dll                      Pattern   : Mapper class                            *
+*  Type     : RecordingDataMapper                        License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Mapping methods for recording data.                                                            *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+namespace Empiria.Land.Registration.Adapters {
+
+  static internal class RecordingDataMapper {
+
+    #region Public methods
+
+    static internal RecordingDataDto MapRecordingData(RecordingAct recordingAct) {
+      RecordingDocument document = recordingAct.Document;
+
+      PhysicalRecording volumeEntry = recordingAct.HasPhysicalRecording ?
+                                            recordingAct.PhysicalRecording : PhysicalRecording.Empty;
+
+      var transaction = recordingAct.Document.GetTransaction();
+
+      return new RecordingDataDto {
+        UID = document.GUID,
+        Type = document.DocumentType.DisplayName,
+        RecordingID = document.UID,
+        Description = volumeEntry.IsEmptyInstance ? document.UID : recordingAct.PhysicalRecording.AsText,
+        Office = document.RecorderOffice.MapToNamedEntity(),
+        VolumeEntryData = MapVolumeEntry(volumeEntry),
+        RecordingTime = document.AuthorizationTime,
+        RecordedBy = document.GetRecordingOfficials()[0].Alias,
+        AuthorizedBy = document.AuthorizedBy.Alias,
+        PresentationTime = document.PresentationTime,
+        TransactionUID = transaction.UID,
+        Status = document.Status.ToString(),
+        Media = InstrumentRecordingMapper.MapStampMedia(document, document.GetTransaction())
+      };
+    }
+
+
+    static private VolumeEntryDataDto MapVolumeEntry(PhysicalRecording volumeEntry) {
+      if (volumeEntry.IsEmptyInstance) {
+        return null;
+      }
+
+      return new VolumeEntryDataDto {
+        UID = volumeEntry.UID,
+        RecordingBookUID = volumeEntry.RecordingBook.UID,
+        InstrumentRecordingUID = volumeEntry.MainDocument.GUID,
+      };
+    }
+
+    #endregion Public methods
+
+  }  // class RecordingDataMapper
+
+}  // namespace Empiria.Land.Registration.Adapters
