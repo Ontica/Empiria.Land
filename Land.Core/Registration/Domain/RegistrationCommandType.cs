e98b7990c68fa0e034a1a1913b2da1ee02e78647
José Manuel Cota <jmcota@ontica.org>
2024-01-30 11:32:19 -0500

    Rfx Merge RegistrationRuleExtensions into RegistrationCommandType

--- Land.Core/Registration/Domain/RegistrationCommandType.cs
+++ Land.Core/Registration/Domain/RegistrationCommandType.cs
@@ -8,6 +8,7 @@
 *                                                                                                            *
 ************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
 
+using System.Collections.Generic;
 using Empiria.Land.RecordableSubjects.Adapters;
 using Empiria.Land.Registration.Adapters;
 
@@ -52,6 +53,103 @@ namespace Empiria.Land.Registration {
 
 
   static internal class RegistrationCommandTypeExtensions {
+
+
+    static internal FixedList<RegistrationCommandType> RegistrationCommandTypes(this RecordingActType recordingActType) {
+      var rule = recordingActType.RecordingRule;
+
+      var list = new List<RegistrationCommandType>();
+
+      if (!rule.IsActive) {
+        list.Add(RegistrationCommandType.Undefined);
+        return list.ToFixedList();
+      }
+
+      switch (rule.AppliesTo) {
+        case RecordingRuleApplication.Association:
+          LoadAssociationCommandTypes(list, rule);
+          break;
+
+        case RecordingRuleApplication.RealEstate:
+        case RecordingRuleApplication.Structure:
+          LoadRealEstateCommandTypes(list, rule);
+          break;
+
+        case RecordingRuleApplication.NoProperty:
+          LoadNoPropertyCommandTypes(list, rule);
+          break;
+
+        case RecordingRuleApplication.RealEstateAct:
+          list.Add(RegistrationCommandType.SelectRealEstateAct);
+          break;
+
+        case RecordingRuleApplication.AssociationAct:
+          list.Add(RegistrationCommandType.SelectAssociationAct);
+          break;
+
+        case RecordingRuleApplication.NoPropertyAct:
+          list.Add(RegistrationCommandType.SelectNoPropertyAct);
+
+          break;
+      }
+
+      if (list.Count == 0) {
+        list.Add(RegistrationCommandType.Undefined);
+      }
+
+      return list.ToFixedList();
+    }
+
+
+    static internal FixedList<RegistrationCommandType> TractIndexRegistrationCommandTypes(this RecordingActType recordingActType) {
+      var rule = recordingActType.RecordingRule;
+
+      var list = new List<RegistrationCommandType>();
+
+      if (!rule.IsActive) {
+        list.Add(RegistrationCommandType.Undefined);
+        return list.ToFixedList();
+      }
+
+      switch (rule.AppliesTo) {
+        case RecordingRuleApplication.Association:
+          list.Add(RegistrationCommandType.AssociationTractIndex);
+          break;
+
+        case RecordingRuleApplication.AssociationAct:
+          list.Add(RegistrationCommandType.AmendAssociationTractIndexAct);
+          break;
+
+
+        case RecordingRuleApplication.NoProperty:
+          list.Add(RegistrationCommandType.NoPropertyTractIndex);
+          break;
+
+        case RecordingRuleApplication.NoPropertyAct:
+          list.Add(RegistrationCommandType.AmendNoPropertyTractIndexAct);
+          break;
+
+        case RecordingRuleApplication.RealEstate:
+        case RecordingRuleApplication.Structure:
+          list.Add(RegistrationCommandType.RealEstateTractIndex);
+          if (rule.AllowPartitions) {
+            list.Add(RegistrationCommandType.RealEstateTractIndexPartition);
+          }
+          break;
+
+        case RecordingRuleApplication.RealEstateAct:
+          list.Add(RegistrationCommandType.AmendRealEstateTractIndexAct);
+          break;
+
+      }
+
+      if (list.Count == 0) {
+        list.Add(RegistrationCommandType.Undefined);
+      }
+
+      return list.ToFixedList();
+    }
+
 
     static internal string Name(this RegistrationCommandType commandType) {
       switch (commandType) {
@@ -275,6 +373,71 @@ namespace Empiria.Land.Registration {
       }
     }
 
+
+    #region Helper Methods
+
+    static private void LoadAssociationCommandTypes(List<RegistrationCommandType> list, RecordingRule rule) {
+      if (SelectUnregistered(rule)) {
+        list.Add(RegistrationCommandType.CreateAssociation);
+      }
+      if (SelectRegistered(rule)) {
+        list.Add(RegistrationCommandType.SelectAssociation);
+      }
+      if (SelectAntecedent(rule)) {
+        list.Add(RegistrationCommandType.SelectAssociationAntecedent);
+      }
+    }
+
+
+    static private void LoadNoPropertyCommandTypes(List<RegistrationCommandType> list, RecordingRule rule) {
+      if (SelectUnregistered(rule)) {
+        list.Add(RegistrationCommandType.CreateNoProperty);
+      }
+      if (SelectRegistered(rule)) {
+        list.Add(RegistrationCommandType.SelectNoProperty);
+      }
+      if (SelectAntecedent(rule)) {
+        list.Add(RegistrationCommandType.SelectNoPropertyAntecedent);
+      }
+    }
+
+
+    static private void LoadRealEstateCommandTypes(List<RegistrationCommandType> list, RecordingRule rule) {
+      if (SelectUnregistered(rule)) {
+        list.Add(RegistrationCommandType.CreateRealEstate);
+      }
+      if (SelectRegistered(rule)) {
+        list.Add(RegistrationCommandType.SelectRealEstate);
+      }
+      if (SelectAntecedent(rule)) {
+        list.Add(RegistrationCommandType.SelectRealEstateAntecedent);
+      }
+      if (rule.AllowPartitions) {
+        list.Add(RegistrationCommandType.CreateRealEstatePartition);
+      }
+      if (rule.AllowPartitions && SelectAntecedent(rule)) {
+        list.Add(RegistrationCommandType.CreateRealEstatePartitionForAntecedent);
+      }
+    }
+
+
+    static private bool SelectAntecedent(RecordingRule rule) {
+      return rule.ResourceRecordingStatus == ResourceRecordingStatus.Antecedent;
+    }
+
+
+    static private bool SelectRegistered(RecordingRule rule) {
+      return rule.ResourceRecordingStatus == ResourceRecordingStatus.Registered ||
+             rule.ResourceRecordingStatus == ResourceRecordingStatus.Both;
+    }
+
+
+    static private bool SelectUnregistered(RecordingRule rule) {
+      return rule.ResourceRecordingStatus == ResourceRecordingStatus.Unregistered ||
+             rule.ResourceRecordingStatus == ResourceRecordingStatus.Both;
+    }
+
+    #endregion Helper Methods
 
   }  // class RegistrationCommandTypeExtensions
 
