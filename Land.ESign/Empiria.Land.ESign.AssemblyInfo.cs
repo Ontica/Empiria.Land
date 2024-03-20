2213fe3c775c266154e96d4d8f6873283cca3cb5
José Manuel Cota <jmcota@ontica.org>
2024-03-20 11:30:25 -0400

    Upgrade Land.ESign project files

---
+++ Land.ESign/Empiria.Land.ESign.AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿/* Empiria Land **********************************************************************************************
+*                                                                                                            *
+*  System   : Empiria Land                                 Module  : Electronic Sign                         *
+*  Assembly : Empiria.Land.ESign.dll                       Pattern : Assembly Attributes File                *
+*                                                          License : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Provides electronic sign services for Empiria Land documents.                                  *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+/*************************************************************************************************************
+* Assembly configuration attributes.                                                                         *
+*************************************************************************************************************/
+[assembly: AssemblyTrademark("Empiria and Ontica are either trademarks of La Vía Óntica SC or Ontica LLC.")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
+[assembly: InternalsVisibleTo("Empiria.Land.Tests")]
