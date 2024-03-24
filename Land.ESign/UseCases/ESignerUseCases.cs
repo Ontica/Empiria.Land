414cbb5f8f5b8ea5f17ce0ed800e977167bef21b
José Manuel Cota <jmcota@ontica.org>
2024-03-24 08:26:53 -0400

    Worklow rules added to ESigner use cases (sign and revoke)

--- Land.ESign/UseCases/ESignerUseCases.cs
+++ Land.ESign/UseCases/ESignerUseCases.cs
@@ -62,9 +62,13 @@ namespace Empiria.Land.ESign.UseCases {
 
       FixedList<LRSTransaction> transactions = command.GetTransactions();
 
+      AssertWorkflowRulesToBeRevoked(transactions);
+
       var signer = new LandDocumentsSigner(command.Credentials);
 
-      signer.RevokeSignForTransactionDocuments(transactions);
+      signer.RevokeTransactionDocumentsSigns(transactions);
+
+      UpdateWorkflowAfterRevoke(transactions);
     }
 
 
@@ -77,10 +81,13 @@ namespace Empiria.Land.ESign.UseCases {
 
       FixedList<LRSTransaction> transactions = command.GetTransactions();
 
+      AssertWorkflowRulesToBeSigned(transactions);
+
       var signer = new LandDocumentsSigner(command.Credentials);
 
       signer.SignTransactionDocuments(transactions);
 
+      UpdateWorkflowAfterSign(transactions);
     }
 
 
@@ -98,10 +105,52 @@ namespace Empiria.Land.ESign.UseCases {
 
     #region Helpers
 
+    private void AssertWorkflowRulesToBeRevoked(FixedList<LRSTransaction> transactions) {
+      foreach (var transaction in transactions) {
+        Assertion.Require(transaction.Workflow.CurrentStatus == TransactionStatus.OnSign ||
+                          transaction.Workflow.CurrentStatus == TransactionStatus.Control ||
+                          transaction.Workflow.CurrentStatus == TransactionStatus.Elaboration ||
+                          transaction.Workflow.CurrentStatus == TransactionStatus.Recording ||
+                          transaction.Workflow.CurrentStatus == TransactionStatus.Revision ||
+                          transaction.Workflow.CurrentStatus == TransactionStatus.Juridic,
+          $"No se puede revocar la firma del trámite {transaction.UID}, debido a que su estado " +
+          $"actual es {transaction.Workflow.CurrentStatus.GetStatusName()}.");
+      }
+    }
+
+
+    private void AssertWorkflowRulesToBeSigned(FixedList<LRSTransaction> transactions) {
+      foreach (var transaction in transactions) {
+        Assertion.Require(transaction.Workflow.CurrentStatus == TransactionStatus.OnSign,
+          $"El trámite {transaction.UID} no se encuentra en estado de firmado. " +
+          $"Su estado actual es {transaction.Workflow.CurrentStatus.GetStatusName()}.");
+
+        Assertion.Require(transaction.ControlData.IsAssignedToUser,
+          $"El trámite {transaction.UID} no está asignado a la persona que está intentando firmarlo.");
+      }
+    }
+
+
     private void PrepareCredentials(ESignCommand command) {
       var entropy = SecurityTokenGenerator.PopToken(command.Credentials, SecurityTokenType.ElectronicSign);
 
       command.Credentials.Password = Cryptographer.Decrypt(command.Credentials.Password, entropy, true);
+    }
+
+
+    private void UpdateWorkflowAfterRevoke(FixedList<LRSTransaction> transactions) {
+      foreach (var transaction in transactions) {
+        if (transaction.Workflow.CurrentStatus == TransactionStatus.OnSign) {
+          transaction.Workflow.ReturnToMe();
+        }
+      }
+    }
+
+
+    private void UpdateWorkflowAfterSign(FixedList<LRSTransaction> transactions) {
+      foreach (var transaction in transactions) {
+        transaction.Workflow.SetNextStatus(TransactionStatus.ToDeliver);
+      }
     }
 
     #endregion Helpers
